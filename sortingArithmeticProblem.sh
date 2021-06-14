b36e100681ddb0949f87c2d6a2a1470851daf799
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-14 13:25:29 +0530

    computing UC2

--- sortingArithmeticProblem.sh
+++ sortingArithmeticProblem.sh
@@ -2,4 +2,5 @@ echo "welcome to sorting arithmetic"
 read -p "Enter first element " a
 read -p "Enter second element " b
 read -p "Enter third element " c
-echo $a $b $c
+result=`echo $a $b $c | awk '{print $1 + $2 * $3 }'`
+echo "result is $result"
