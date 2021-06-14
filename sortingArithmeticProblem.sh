b045d44d9dea4e40cdc4ba89fee46094765922b3
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-14 14:21:25 +0530

    storing the values in array

--- sortingArithmeticProblem.sh
+++ sortingArithmeticProblem.sh
@@ -1,12 +1,21 @@
+declare -A arr
 echo "welcome to sorting arithmetic"
 read -p "Enter first element " a
 read -p "Enter second element " b
 read -p "Enter third element " c
-result1=`echo $a $b $c | awk '{print $1 + $2 * $3 }'`
-echo "result 1 is $result1"
-result2=`echo $a $b $c | awk '{print $1 * $2 + $3 }'`
-echo "result 2 is $result2"
-result3=`echo $c $a $b | awk '{print $1 + $2 / $3 }'`
-echo "result 3 is $result3"
-result4=`echo $a $b $c | awk '{print $1 % $2 + $3 }'`
-echo "result 4 is $result4"
+
+	result1=`echo $a $b $c | awk '{print $1 + $2 * $3 }'`
+	echo "result 1 is $result1"
+	arr[0]=$result1
+	result2=`echo $a $b $c | awk '{print $1 * $2 + $3 }'`
+	echo "result 2 is $result2"
+	arr[1]=$result2
+	result3=`echo $c $a $b | awk '{print $1 + $2 / $3 }'`
+	echo "result 3 is $result3"
+	arr[2]=$result3
+	result4=`echo $a $b $c | awk '{print $1 % $2 + $3 }'`
+	echo "result 4 is $result4"
+	arr[3]=$result4
+
+echo ${arr[@]}
+
